70639407c022acc433d43f6095f90e016007d6af
12penaaariel01 <12penaaariel01@gmail.com>
2026-06-08 22:43:05 -0300

    fix next

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
+  // Forzamos a Next.js a mapear internamente la raíz si el compilador automático falla
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/page', // Apuesta segura al archivo físico
+      },
+    ]
+  },
 }
 
 export default nextConfig
